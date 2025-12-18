6b6e3cc9af7512a0b032f207efe80eec444455a5
barrosfelip <31870378+barrosfelip@users.noreply.github.com>
2025-12-18 11:41:44 -0300

    Add files via upload

---
+++ site-casamento/js/script.js
@@ -0,0 +1,5 @@
+document.querySelectorAll(".gift-card button").forEach(button => {
+  button.addEventListener("click", () => {
+    alert("Em breve você poderá escolher Pix, Cartão ou Boleto 💳");
+  });
+});
