3040f6a7a8e69cbe120de680c45799ddf695d92d
barrosfelip <31870378+barrosfelip@users.noreply.github.com>
2025-12-18 11:42:14 -0300

    Delete site-casamento directory

--- site-casamento/js/script.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll(".gift-card button").forEach(button => {
-  button.addEventListener("click", () => {
-    alert("Em breve você poderá escolher Pix, Cartão ou Boleto 💳");
-  });
-});
