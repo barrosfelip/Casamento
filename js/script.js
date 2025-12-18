6ff778a4873a5570e08e1013b85b95c1c4d49520
barrosfelip <31870378+barrosfelip@users.noreply.github.com>
2025-12-18 14:42:51 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,10 @@ document.querySelectorAll(".gift-card button").forEach(button => {
     alert("Em breve você poderá escolher Pix, Cartão ou Boleto 💳");
   });
 });
+
+function pagar(valor) {
+  alert(
+    "Valor selecionado: R$ " + valor.toFixed(2) +
+    "\n\nEm breve você poderá pagar via Pix, Cartão ou Boleto 💳"
+  );
+}
